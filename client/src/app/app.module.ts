eba159d54ba02542a92889d52c866c1aad3fd544
Amr <merodiro@gmail.com>
2017-06-09 21:59:13 +0200

    add register logic

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProfileComponent } from './profile/profile.component';
 
+import { AuthService } from './auth.service'
 
 
 @NgModule({
@@ -34,7 +35,7 @@ import { ProfileComponent } from './profile/profile.component';
     AppRoutingModule,
     FlashMessagesModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { FlashMessagesService } from 'angular2-flash-messages';
-
+import { AuthService } from '../auth.service';
 
 export interface User {
   name: String;
@@ -21,7 +22,12 @@ export class RegisterComponent implements OnInit {
 
   registerForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private flashMessages: FlashMessagesService) { }
+  constructor(
+    private fb: FormBuilder,
+    private flashMessages: FlashMessagesService,
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.createForm()
@@ -39,6 +45,16 @@ export class RegisterComponent implements OnInit {
   register({ value, valid }: { value: User, valid: boolean }) {
     if(!valid) {
       this.flashMessages.show('Please fill all fields', { cssClass: 'alert-danger', timeOut: 3000 })
+      return false;
     }
+
+    this.authService.register(value).subscribe(res => {
+      if(res.success) {
+        this.flashMessages.show('you are now registered and can login', { cssClass: 'alert-success', timeOut: 3000 })
+        this.router.navigate(['/login'])
+      } else {
+        this.flashMessages.show('SOmething went wrong', { cssClass: 'alert-danger', timeOut: 3000 })
+      }
+    });
   }
 }
